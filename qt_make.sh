0956772609c9f5a587de81dbafc512646df6c96c
SatoshiShimada <mylinux1204@gmail.com>
2016-03-05 15:55:12 +0900

    Edit make rule for Qt4

--- qt_make.sh
+++ qt_make.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
 ###################
-# HOW TO INSTALL
+# HOW TO INSTALL Qt4
 # on Ubuntu 14.04
 ###################
 # execute follow command:
+#sudo apt-get update && sudo apt-get upgrade
 #sudo apt-get install libqt4-core libqt4-dev libqt4-gui qt4-dev-tools
 
-if [ -z $1 ] ;then
-	echo "argument too few."
-	exit 1
-fi
-
-case "$1" in
-	"qmake" )
-		#qmake -project
-		#qmake
-
-		qmake-qt4 -project
-		qmake-qt4
-		;;
-	"make" )
-		make
-		mv ./interface ../exe
-		;;
-esac
-
-exit 0
+qmake-qt4 -project
+qmake-qt4
 
