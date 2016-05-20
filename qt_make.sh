f8d56f499ce5f0f59919240ac2660450093a9520
satoshi <mylinux1204@gmail.com>
2016-05-20 12:24:53 +0900

    Add message for build on ubuntu 12.04

--- qt_make.sh
+++ qt_make.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Requirement
+# * g++ (version > 4.8)
+# * qt4
+
 ###################
 # HOW TO INSTALL Qt4
 # on Ubuntu 14.04
@@ -7,6 +11,14 @@
 # execute follow command:
 #sudo apt-get update && sudo apt-get upgrade
 #sudo apt-get install libqt4-core libqt4-dev libqt4-gui qt4-dev-tools
+
+# INSTALL g++ version 4.8
+# on Ubuntu 12.04
+#sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+#sudo apt-get update
+#sudo apt-get install gcc-4.8 g++-4.8
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 
 PROJECT='GameMonitor.pro'
 
