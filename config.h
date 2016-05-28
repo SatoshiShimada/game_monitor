3ba04e0d40dfb62b2ce9899d0ebde028699978cd
satoshi <satoshi@satoshi-no-MacBook-Air.local>
2016-05-28 11:14:37 +0900

    Support build on MacOSX

--- config.h
+++ config.h
@@ -16,3 +16,4 @@ private:
 };
 
 #endif // _CONFIG_H_
+
--- qt_make.sh
+++ qt_make.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+###################
 # Requirement
+###################
 # * g++ (version > 4.8)
 # * qt4
+# * boost
 
 ###################
 # HOW TO INSTALL Qt4
@@ -12,19 +15,36 @@
 #sudo apt-get update && sudo apt-get upgrade
 #sudo apt-get install libqt4-core libqt4-dev libqt4-gui qt4-dev-tools
 
-# INSTALL g++ version 4.8
+###################
+# HOW TO INSTALL g++ version 4.8
 # on Ubuntu 12.04
+###################
 #sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 #sudo apt-get update
 #sudo apt-get install gcc-4.8 g++-4.8
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
 #sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 
+###################
+# HOW TO BUILD on MacOSX
+# install qt4 and boost
+###################
+#brew update -all
+#brew install qt4
+#brew install boost
+#qt_make.sh
+
 PROJECT='GameMonitor.pro'
 
-qmake-qt4 -project -o $PROJECT
+if [ $(which qmake-qt4) ]; then
+	QMAKE='qmake-qt4'
+else
+	QMAKE='qmake'
+fi
+echo $QMAKE
+$QMAKE -project -o $PROJECT
 echo 'QMAKE_CXXFLAGS += --std=c++11' >> $PROJECT
 echo 'unix:LIBS += -lboost_system' >> $PROJECT
-qmake-qt4
+$QMAKE
 make all -j4
 
