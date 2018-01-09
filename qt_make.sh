d09981052ec0c90cde8bcb8743192a415055ca9a
satoshi <mylinux1204@gmail.com>
2018-01-09 16:25:17 +0900

    building in build directory

--- qt_make.sh
+++ qt_make.sh
@@ -38,6 +38,16 @@ echo $($QMAKE --version)
 $QMAKE -project -o $PROJECT
 echo 'QMAKE_CXXFLAGS += --std=c++11' >> $PROJECT
 echo 'QT += network widgets' >> $PROJECT
+
+if [ ! -d build ]; then
+	mkdir build
+fi
+echo 'DESTDIR = build' >> $PROJECT
+echo 'OBJECTS_DIR = build/obj' >> $PROJECT
+echo 'MOC_DIR = build/moc' >> $PROJECT
+echo 'RCC_DIR = build/rcc' >> $PROJECT
+echo 'UI_DIR = build/ui' >> $PROJECT
+
 $QMAKE
 make all -j8
 
