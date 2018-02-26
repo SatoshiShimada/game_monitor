b1cfc2d90f203d2ad2bc1e7293a4512d1bb594d4
satoshi <mylinux1204@gmail.com>
2018-02-27 00:18:59 +0900

    add some code in game_monitor.h

--- src/game_monitor.h
+++ src/game_monitor.h
@@ -1,14 +1,40 @@
 #ifndef _GAME_MONITOR_H_
 #define _GAME_MONITOR_H_
 
+#include <vector>
+#include "<types.h"
+
 class GameMonitor {
 public:
 	GameMonitor() {}
 	~GameMonitor() {}
-	void UpdateRobotState(int);
+public slots:
+	void setRobotState(int);
+	void SetGameState(void);
+signals:
+	void UpdateRobotState(void);
 	void UpdateGameState(void);
 private:
+	std::vector<RobotState> robot_state;
+	GameState game_state;
+	std::vector<UdpServer *> udp_thread;
 };
 
 #endif // _GAME_MONITOR_H_
+
+GameMonitor::GameMonitor()
+{
+	robot_state(6);
+	for(int i = 0; i < 6; i++) {
+		udp_thread.push_back(new UdpServer(id));
+		connect(*udp_thread[i], SIGNAL(received()), robot_state[i], SLOT(setData()));
+		connect(robot_state[i], SIGNAL(updateState(int)), this, SLOT(setRobotState(int)));
+	}
+	connect(game_state, SIGNAL(updateState()), this, SLOT(setGameState()));
+}
+
+void GameMonitor::setRobotState(int id)
+{
+	emit(updateRobotState());
+}
 
